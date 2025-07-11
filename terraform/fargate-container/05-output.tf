2eb2a678557f6ac56ed45122e03fbac8d7c83fb0
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-07-11 15:17:46 -0300

    rename file

--- terraform/fargate-container/05-output.tf
+++ terraform/fargate-container/05-output.tf
@@ -1,19 +1,19 @@
 output "ecs_task_definition_arn" {
-  value = aws_ecs_task_definition.server.arn
+  value = aws_ecs_task_definition.server
 }
 
 output "ecs_service_name" {
-  value = aws_ecs_service.server.name
+  value = aws_ecs_service.server
 }
 
 output "cloudwatch_log_group_name" {
-  value = aws_cloudwatch_log_group.default.name
+  value = aws_cloudwatch_log_group.default
 }
 
 output "iam_task_execution_role_arn" {
-  value = aws_iam_role.task-execution.arn
+  value = aws_iam_role.task-execution
 }
 
 output "iam_service_role_arn" {
-  value = aws_iam_role.service.arn
+  value = aws_iam_role.service
 }
