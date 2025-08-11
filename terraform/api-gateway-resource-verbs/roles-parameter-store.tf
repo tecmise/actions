09c4ee9959ca5c99234943c9f7ffc72ab5895ef6
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-08-11 11:54:06 -0300

    fix value array

--- terraform/api-gateway-resource-verbs/roles-parameter-store.tf
+++ terraform/api-gateway-resource-verbs/roles-parameter-store.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "roles_parameter" {
   name = "/api-gateway/${var.rest_api_id}/${var.resource_id}/${var.verb}/roles"
   type = "StringList"
-  value = var.roles
+  value = join(",", var.roles)
 }
 
