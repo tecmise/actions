f6c19cc7052120b5dff4ac4e4c2a7f7598c24d71
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-10-17 11:49:16 -0300

    health check test

--- terraform/lambda_health/variable.tf
+++ terraform/lambda_health/variable.tf
@@ -21,3 +21,26 @@ variable "lambda_arn" {
 variable "function_name" {
   type = string
 }
+
+variable "payload" {
+  type = object({
+    resource     = optional(string, "/health/check"),
+    path         = optional(string, "/health/check"),
+    httpMethod   = optional(string, "GET"),
+    headers      = optional(map(string), {
+      Accept = "*/*",
+    }),
+    resourcePath = optional(string, "/health/check"),
+    body         = optional(string, "/health/check"),
+  })
+  default = {
+    resource     = "/health/check",
+    path         = "/health/check",
+    httpMethod   = "GET",
+    headers      = {
+      Accept = "*/*",
+    },
+    resourcePath = "/health/check",
+    body         = "",
+  }
+}
