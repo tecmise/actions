40b51387a5ea5f67433199e02c9a868e9466ddeb
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-10-31 12:01:09 -0300

    change steps

--- terraform/assync-event-channel/local.tf
+++ terraform/assync-event-channel/local.tf
@@ -2,10 +2,10 @@ locals {
   content = endswith(var.file_name, ".json") ? jsondecode(file("content.json")) : yamldecode(file("content.yaml"))
 
   topics_fifo = {
-    for queue in local.content["entities"] : queue["name"] => queue
+    for queue in local.content["publishers"] : queue["name"] => queue
   }
   parameters = flatten([
-    for value in local.content["entities"] : flatten([
+    for value in local.content["publishers"] : flatten([
       for key, attr in (value["attributes"] != null ? value["attributes"] : []) : {
         name: "/${var.application}/assync-event-channel/${value["name"]}/${attr["name"]}",
         value: attr["value"]
