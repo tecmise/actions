76008900ffcf49bcedcf64b55a6806bce23f0bd7
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-10-31 15:46:23 -0300

    contains key

--- terraform/assync-event-channel/parameters.tf
+++ terraform/assync-event-channel/parameters.tf
@@ -1,6 +1,6 @@
-resource "aws_ssm_parameter" "parameter" {
-  count = length(local.parameters)
-  name = local.parameters[count.index]["name"]
-  type = "String"
-  value = local.parameters[count.index]["value"]
-}
+# resource "aws_ssm_parameter" "parameter" {
+#   count = length(local.parameters)
+#   name = local.parameters[count.index]["name"]
+#   type = "String"
+#   value = local.parameters[count.index]["value"]
+# }
