fb1ad10bc5048b069bc7d37fd6bfc70e06be8b04
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-11-18 10:17:15 -0300

    fixed

--- terraform/fargate-container/04-service.tf
+++ terraform/fargate-container/04-service.tf
@@ -1,3 +1,72 @@
+resource "aws_ecs_service" "server" {
+  name             = var.application_name
+  cluster          = var.cluster_id
+  task_definition  = aws_ecs_task_definition.server.arn
+  desired_count    = var.desired_count
+  launch_type      = var.launch_type
+  platform_version = var.launch_type == "FARGATE" ? "LATEST" : null
+  enable_execute_command = true
+
+  dynamic "service_connect_configuration" {
+    for_each = toset(var.service_connect_configuration)
+    content {
+      enabled = service_connect_configuration.value.enabled
+      namespace = service_connect_configuration.value.namespace
+
+      service {
+        port_name = service_connect_configuration.value.service["port_name"]
+        discovery_name = service_connect_configuration.value.service["discovery_name"]
+
+        dynamic "client_alias" {
+          for_each = service_connect_configuration.value.service["client_alias"]
+          content {
+            port = client_alias.value["port"]
+            dns_name = client_alias.value["dns_name"]
+          }
+        }
+      }
+    }
+  }
+
+  dynamic "service_registries" {
+    for_each = toset(var.service_registries)
+    content {
+      registry_arn = service_registries.value.registry_arn
+    }
+  }
+
+  dynamic "capacity_provider_strategy" {
+    for_each = toset(var.capacity_provider_strategy)
+    content {
+      capacity_provider = capacity_provider_strategy.value.capacity_provider
+      weight = capacity_provider_strategy.value.weight
+      base = capacity_provider_strategy.value.base
+    }
+  }
+
+  dynamic "network_configuration" {
+    for_each = toset(var.network_configuration)
+    content {
+      subnets         = network_configuration.value.subnets
+      security_groups = network_configuration.value.security_groups
+      assign_public_ip = network_configuration.value.assign_public_ip
+    }
+  }
+
+  dynamic "load_balancer" {
+    for_each = toset(var.load_balancer)
+    content {
+     target_group_arn = load_balancer.value.target_group_arn
+     container_name   = load_balancer.value.container_name
+     container_port   = load_balancer.value.container_port
+    }
+  }
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+}
+
+
 resource "aws_ecs_task_definition" "server" {
   family                   = var.family_name
   network_mode             = var.network_mode
@@ -33,66 +102,4 @@ resource "aws_ecs_task_definition" "server" {
 
 }
 
-resource "aws_ecs_service" "server" {
-  name             = var.application_name
-  cluster          = var.cluster_id
-  task_definition  = aws_ecs_task_definition.server.arn
-  desired_count    = var.desired_count
-  launch_type      = var.launch_type
-  platform_version = var.launch_type == "FARGATE" ? "LATEST" : null
-  enable_execute_command = true
-
-
-  service_connect_configuration {
-    enabled = false
-    namespace = "shield.local"
-    service {
-      port_name = "native"
-      discovery_name = "clickhouse"
-      client_alias {
-        port = 9000
-        dns_name = "clickhouse-service"
-      }
-    }
-  }
-
-  dynamic "service_registries" {
-    for_each = toset(var.service_registries)
-    content {
-      registry_arn = service_registries.value.registry_arn
-    }
-  }
-
-
-
-  dynamic "capacity_provider_strategy" {
-    for_each = toset(var.capacity_provider_strategy)
-    content {
-      capacity_provider = capacity_provider_strategy.value.capacity_provider
-      weight = capacity_provider_strategy.value.weight
-      base = capacity_provider_strategy.value.base
-    }
-  }
-
-  dynamic "network_configuration" {
-    for_each = toset(var.network_configuration)
-    content {
-      subnets         = network_configuration.value.subnets
-      security_groups = network_configuration.value.security_groups
-      assign_public_ip = network_configuration.value.assign_public_ip
-    }
-  }
-
-  dynamic "load_balancer" {
-    for_each = toset(var.load_balancer)
-    content {
-     target_group_arn = load_balancer.value.target_group_arn
-     container_name   = load_balancer.value.container_name
-     container_port   = load_balancer.value.container_port
-    }
-  }
-
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
-}
 
