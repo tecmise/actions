264abde299923142edb2752f1f5b71c6b5536793
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-11-18 03:05:38 -0300

    Service conenctor

--- terraform/fargate-container/04-service.tf
+++ terraform/fargate-container/04-service.tf
@@ -41,23 +41,28 @@ resource "aws_ecs_service" "server" {
   launch_type      = var.launch_type
   platform_version = var.launch_type == "FARGATE" ? "LATEST" : null
 
+
   dynamic "service_connect_configuration" {
     for_each = toset(var.service_connect_configuration)
     content {
-      enabled = true
+      enabled = service_connect_configuration.value.enabled
       namespace = service_connect_configuration.value.namespace
-      service = {
+
+      service {
         port_name = service_connect_configuration.value.service["port_name"]
         discovery_name = service_connect_configuration.value.service["discovery_name"]
-        client_alias = flatten([
-          for alias in service_connect_configuration.value.service["client_alias"] : {
-            port = alias["port"]
-            dns_name  = alias["dns_name"]
+
+        dynamic "client_alias" {
+          for_each = service_connect_configuration.value.service["client_alias"]
+          content {
+            port = client_alias.value["port"]
+            dns_name = client_alias.value["dns_name"]
           }
-        ])
+        }
       }
     }
   }
+
 
   dynamic "service_registries" {
     for_each = toset(var.service_registries)
