9af07749a8b5089b784010feab235395c75d79f2
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2025-12-07 10:43:41 -0300

    module redis

---
+++ terraform/redis/helm.tf
@@ -0,0 +1,70 @@
+//https://artifacthub.io/packages/helm/bitnami/redis
+resource "helm_release" "redis" {
+  name       = var.name
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "redis"
+  version    = "24.0.0"
+  namespace  = var.namespace
+  create_namespace = true
+  recreate_pods = true
+  timeout = 600
+  set = [
+    {
+      name  = "architecture"
+      value = "standalone"
+    },
+    {
+      name  = "metrics.enabled"
+      value = "true"
+    },
+    {
+      name  = "metrics.serviceMonitor.enabled"
+      value = "true"
+    },
+    {
+      name  = "metrics.serviceMonitor.namespace"
+      value = var.namespace
+    },
+    {
+      name  = "replica.nodeSelector.nodegroup-type"
+      value = var.node_selector
+    },
+    {
+      name  = "master.nodeSelector.nodegroup-type"
+      value = var.node_selector
+    },
+    {
+      name  = "master.resources.limits.cpu"
+      value = var.limits_cpu
+    },
+    {
+      name  = "master.resources.requests.cpu"
+      value = var.requests_cpu
+    },
+    {
+      name  = "master.resources.limits.memory"
+      value = var.limits_memory
+    },
+    {
+      name  = "master.resources.requests.memory"
+      value = var.requests_memory
+    },
+    {
+      name  = "master.persistence.enabled"
+      value = var.enabled_persistence
+    },
+    {
+      name  = "replica.persistence.enabled"
+      value = var.enabled_persistence
+    },
+    {
+      name = "master.persistence.size"
+      value = var.persistence_size
+    },
+    {
+      name = "replica.persistence.size"
+      value = var.persistence_size
+    }
+  ]
+}
+
---
+++ terraform/redis/variables.tf
@@ -0,0 +1,41 @@
+variable "name" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "node_selector" {
+  type = string
+}
+
+variable "limits_cpu" {
+  type = string
+  default = "100m"
+}
+
+variable "requests_cpu" {
+  type = string
+  default = "50m"
+}
+
+variable "limits_memory" {
+  type = string
+  default = "128Mi"
+}
+
+variable "requests_memory" {
+  type = string
+  default = "64Mi"
+}
+
+variable "enabled_persistence" {
+  type = string
+  default = false
+}
+
+variable "persistence_size" {
+  type = string
+  default = "1Gi"
+}
