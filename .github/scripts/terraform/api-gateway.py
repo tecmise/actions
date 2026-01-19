ad9c5d3f33e68a586a835950e4af79e4996f9cec
GuilhermeGiusti <guilherme.giusti04@gmail.com>
2026-01-19 13:34:58 -0300

    starts with

--- .github/scripts/terraform/api-gateway.py
+++ .github/scripts/terraform/api-gateway.py
@@ -88,7 +88,7 @@ def create_terraform_file(routes: Dict[str, Route], key: str, route: Route, vpc_
                 print(f" ", file=f)
 
                 print(f"module \"{route.id}_{method.name.lower()}\" {{ ", file = f)
-                print(f"   source                                       = \"git::https://github.com/tecmise/actions//terraform/api-gateway-resource-verbs?ref=v6.2.5\"", file = f)
+                print(f"   source                                       = \"git::https://github.com/tecmise/actions//terraform/api-gateway-resource-verbs?ref=main\"", file = f)
                 print(f"   resource_id                                  = aws_api_gateway_resource.{route.id}.id ", file=f)
                 print(f"   rest_api_id                                  = aws_api_gateway_resource.{route.id}.rest_api_id ", file=f)
                 print(f"   verb                                         = \"{method.name}\" ", file=f)
